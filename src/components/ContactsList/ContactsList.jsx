d3cec54786d00eb6211efcd5e707055ec6b13115
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-11-09 18:19:29 +0200

    update selectors
    add getVisibleContacts = createSelector

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,24 +1,16 @@
 import { Contact } from './ContactsList.styled';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectContacts, selectStatusFilter } from 'redux/selectors';
+import { getVisibleContacts } from 'redux/selectors';
 import { removeContact } from 'redux/operations';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const statusFilter = useSelector(selectStatusFilter);
 
   const deleteContact = contactId => {
     dispatch(removeContact(contactId));
   };
 
-  const getVisibleContacts = () => {
-    const normalizedFilter = statusFilter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-  const visibleContacts = getVisibleContacts();
+  const visibleContacts = useSelector(getVisibleContacts);
   return (
     <ul>
       {visibleContacts.map(({ id, name, phone }) => {
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,17 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 // Filter
 export const selectStatusFilter = state => state.filters;
+
+export const getVisibleContacts = createSelector(
+  [selectContacts, selectStatusFilter],
+  (contacts, statusFilter) => {
+    const normalizedFilter = statusFilter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
